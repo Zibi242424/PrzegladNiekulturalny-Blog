2bc19bbfefe6be9db16e63725fe67667ebab6ce6
Zibi242424 <manselzbigniew@gmail.com>
2017-09-26 00:04:15 +0200

    Corrected db_create

--- db_create.py
+++ db_create.py
@@ -3,6 +3,7 @@ from models import BlogPost
 from datetime import datetime
 from pytz import timezone
 
+poland = timezone('Europe/Warsaw')
 date = str(datetime.now(poland))[:-13] 
 
 db.create_all()
