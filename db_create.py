3ba246618f4985c7a4229c86025722b031c80ffe
Zibi242424 <manselzbigniew@gmail.com>
2017-09-25 23:59:48 +0200

    Added db_create

--- db_create.py
+++ db_create.py
@@ -1,8 +1,12 @@
 from app import db
 from models import BlogPost
+from datetime import datetime
+from pytz import timezone
+
+date = str(datetime.now(poland))[:-13] 
 
 db.create_all()
 
-db.session.add(BlogPost("im good", "well", "asd"))
+db.session.add(BlogPost("Hi Zuzia!", "This is the first post", date))
 
 db.session.commit()
