2a9e8675c5ff3ccaf9b229cb456b967e9cd82fdb
Zibi242424 <manselzbigniew@gmail.com>
2017-09-28 17:36:15 +0200

    changed config and db_create

--- db_create.py
+++ db_create.py
@@ -8,9 +8,7 @@ date = str(datetime.now(poland))[:-13]
 
 db.create_all()
 
-db.session.add(BlogPost("Kat. film1", "jakis header", "post 1", date, "Film"))
-db.session.add(BlogPost("Kat. film2", "inny header", "post 2", date, "Muzyka"))
-db.session.add(BlogPost("Kat. film3", "jeszce inny", "post 2", date, "Film"))
-db.session.add(BlogPost("Kat. film4", "header be sensu", "post 3", date, "Film"))
+db.session.add(BlogPost("Post testowy", "Sprawdzamy czy wszystko gra", "Mam nadzieję że tak", date, "Film"))
+
 
 db.session.commit()
